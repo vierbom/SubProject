d48af30587f61fe65c6ca53673260c9a6b86ff2e
EX-CHENWANTONG001 <vierbom@gmail.com>
2016-08-31 12:27:40 +0800

    <E><提高版本>

--- app/src/main/java/com/vierbom/gooddoc/MainActivity.java
+++ app/src/main/java/com/vierbom/gooddoc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.vierbom.gooddoc;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,14 +14,23 @@ import xiaofei.library.hermeseventbustest.Dragon;
 
 public class MainActivity extends AppCompatActivity {
     private TextView viewById;
+    private Handler ttsHandler;
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            HermesEventBus.getDefault().post(1);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HandlerThread mHandlerThread = new HandlerThread("MainActivity");
+        mHandlerThread.start();
+        ttsHandler = new Handler(mHandlerThread.getLooper());
         HermesEventBus.getDefault().register(this);
         setContentView(R.layout.activity_main);
         viewById = (TextView) findViewById(R.id.tv);
         /**请求主项目事件*/
-        HermesEventBus.getDefault().post(1);
+//        ttsHandler.postDelayed(runnable, 500);
     }
 
     @Override
@@ -33,5 +44,17 @@ public class MainActivity extends AppCompatActivity {
     public void showText(Dragon dragon) {
         /**获取从主项目返回的Pokemon的名字*/
         viewById.setText(dragon.getName());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void showText(String dragon) {
+        /**获取从主项目返回的Pokemon的名字*/
+        viewById.setText(dragon);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void showText(Integer dragon) {
+        /**获取从主项目返回的Pokemon的名字*/
+        viewById.setText(String.valueOf(dragon));
     }
 }
